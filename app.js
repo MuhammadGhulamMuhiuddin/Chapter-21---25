8a03445b223155b14e281712748d5e6a3e249af2
MuhammadGhulamMuhiuddin <muhammadghulammuhiuddin791@gmail.com>
2024-10-05 15:21:23 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,124 @@
+// Question No.1
+
+
+// var firstName = prompt("Enter Your First Name.Please");
+// var lastName = prompt("Enter Your Last Name.Please");
+
+// var fullName = firstName + " " + lastName;
+
+// console.log("Hey!", fullName);
+
+// Question No.2
+
+// var phone = prompt("Enter your Favourite Phone Model!");
+
+// var phoneLenght = phone.length;
+
+// console.log("Your Favorite is: " + phone + "And Your Phone Lenght is: " + phoneLenght );
+
+
+// Question No.3
+
+// var str = "Pakistani ";
+// console.log("The String is: " + str + "And the index of n is: " + str.indexOf("n"));
+
+// Question No.4
+
+// var str = "Hello World ";
+// console.log("The String is: " + str + "And the lastIndex of n is: " + str.lastIndexOf("l"));
+
+// Question No.5
+
+// var str = "Pakistani ";
+// console.log("The String is: " + str + "And the character of index is: " + str.charAt("3"));
+
+
+// Question No.6
+
+// Question No.7
+// var city = "Hyderabad ";
+// console.log("The city before is: " + city + "And the city after is: " + city.replace("Hydera","Islama"));
+
+// Question No.8
+// var message = ("Ali and Sami are best friends. They play cricket and football together.");
+// console.log(message.replaceAll("and","&"));
+
+
+// Question No.9
+
+// Question No.10
+
+
+
+// var useri prompt("user1" "0 or 1")
+
+// var user2 prompt("user2: "8 or 1")
+
+
+// var num Math.floor(Math.random()*2)
+// if (user1 || user2 == num){
+
+//
+
+// if(user1== num){
+
+
+//     //
+    
+//     console.log(num" User1 Win")
+    
+    
+    
+//     }else{
+    
+    
+    
+//     //
+    
+//     console.log(num+" User2 Win")
+    
+//      //
+    
+    
+    
+//     //)
+    
+    
+    
+    
+    
+//     I
+    
+    
+//     // if (user == num) (
+    
+//     / console.log("num You Win");
+    
+//     // var num = Math.floor(Math.random()*7)
+    
+//     // var user = prompt("Select any no. from 1-6");
+    
+    
+//     //} else {
+    
+//     console.log("num Computer Win");
+    
+    
+//     //}
+    
+    
+//     K
+    
+//     25
+    
+//     26
+    
+//     var num Math.floor(Math.random()*7)
+    
+//     console.log ("Your dice Roll and apper+num");
+
+
+
+
+
+
